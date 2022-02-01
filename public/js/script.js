752fb131c1a81b6dfca57d9507e1ed2559631f5c
Mar <5052218+Mardahak@users.noreply.github.com>
2022-02-01 01:01:21 -0500

    Added animations & simple js for hamburger menu

---
+++ public/js/script.js
@@ -0,0 +1,23 @@
+const btnHamburger = document.querySelector('#btnHamburger');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelectorAll('.has-fade');
+
+header.addEventListener ('click', function(){
+    console.log('open hamburger');
+
+    if(header.classList.contains('open')){ //Close Hamburgie
+        header.classList.remove('open');
+        fadeElems.forEach(function(element){
+            element.classList.remove('fade-in');
+            element.classList.add('fade-out');
+        });
+    }
+    else { //Open Hamburgie
+        header.classList.add('open');
+        fadeElems.forEach(function(element){
+            element.classList.remove('fade-out');
+            element.classList.add('fade-in');
+        });
+    }
+});
