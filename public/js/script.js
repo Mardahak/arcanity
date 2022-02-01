0c5fde09318a04ae953beb2f0ffed42bbb29bbae
Mar <5052218+Mardahak@users.noreply.github.com>
2022-02-01 07:05:26 -0500

    hamburger menu finished!

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,5 @@
 const btnHamburger = document.querySelector('#btnHamburger');
+const body = document.querySelector('body')
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
 const fadeElems = document.querySelectorAll('.has-fade');
@@ -8,12 +9,14 @@ header.addEventListener ('click', function(){
 
     if(header.classList.contains('open')){ //Close Hamburgie
         header.classList.remove('open');
+        body.classList.remove('no-scroll');
         fadeElems.forEach(function(element){
             element.classList.remove('fade-in');
             element.classList.add('fade-out');
         });
     }
     else { //Open Hamburgie
+        body.classList.add('no-scroll');
         header.classList.add('open');
         fadeElems.forEach(function(element){
             element.classList.remove('fade-out');
